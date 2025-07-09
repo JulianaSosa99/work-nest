2283fc4e980135fa981fd8c1b52efb57b25a7f8d
Juliana Sosa <166420351+JulianaSosa99@users.noreply.github.com>
2025-07-09 00:09:35 -0500

    Update CorsConfig.java

--- login/src/main/java/com/login/login/Configuration/CorsConfig.java
+++ login/src/main/java/com/login/login/Configuration/CorsConfig.java
@@ -13,6 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                        .allowedOrigins("https://front-worknest.onrender.com")
                         .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
